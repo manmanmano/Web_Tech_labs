5fee9d4d5f46753a9dc43627f241d0241a917570
Mariano <madang@taltech.ee>
2021-03-08 17:04:46 +0200

    added download. syntax fixed. form handling done

--- lab_05/register.php
+++ lab_05/register.php
@@ -2,45 +2,48 @@
 
 $file = 'csvfile/data.csv'; 
 
-if (preg_match("/^[a-zA-Z']+$/", $_POST['first-name'])) {
-	$first = $_POST['first-name']; . chr(44);
-} else {
-	die('Invalid first name given!');
+if (isset($_POST['submit'])) {
+    if (preg_match("/^[a-zA-Z' ]+$/", $_POST['first-name'])) {
+	    $first = $_POST['first-name'] . chr(44);
+    } else {
+	    die('Invalid first name given!');
+    }
+
+    if (preg_match("/^[a-zA-Z' ]+$/", $_POST['middle-name'])) {
+	$middle = $_POST['middle-name'] . chr(44);
+    } else {
+	    die('Invalid second name given!');
+    }
+
+    if (preg_match("/^[a-zA-Z' ]+$/", $_POST['last-name'])) {
+	    $last = $_POST['last-name'] . chr(44);
+    } else {
+	    die('Invalid last name given!');
+    }   
+
+    $salutation = $_POST['salutation-select'] . chr(44); 
+
+    if ($_POST['age'] > 17 && is_numeric($_POST['age'])) {
+	    $age = $_POST['age'] . chr(44);
+    } else {
+	    die('Invalid age given!'); 
+    }
+
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $email = $_POST['email'] . chr(44);
+    } else {
+        die('Invalid email given!');
+    }
+
+    $phone = $_POST['contact-phone'] . chr(44);
+
+    $arrival = $_POST['arrival'] . PHP_EOL;
+
+    $data = $names . $salutation . $age . $email . $phone . $arrival; 
+    echo 'Registration confirmed!' . PHP_EOL . 'Registered information: ' . $data;
+
+    file_put_contents($file, $data, FILE_APPEND);
 }
-
-if (preg_match("/^[a-zA-Z']+$/", $_POST['middle-name'])) {
-	$middle = $_POST['middle-name']; . chr(44);
-} else {
-	die('Invalid second name given!');
-}
-
-if (preg_match("/^[a-zA-Z']+$/", $_POST['last-name'])) {
-	$last = $_POST['last-name']; . chr(44);
-} else {
-	die('Invalid last name given!');
-}
-
-$salutation = $_POST['salutation-select'] . chr(44); 
-
-if ($_POST['age'] > 17 && is_numeric($_POST['age'])) {
-	$age = $_POST['age'] . chr(44);
-} else {
-	die('Invalid age given!'); 
-}
-
-if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    $email = $_POST['email'] . chr(44);
-} else {
-    die('Invalid email given!');
-}
-
-$phone = $_POST['contact-phone'] . chr(44);
-
-$arrival = $_POST['arrival'] . PHP_EOL;
-
-$data = $names . $salutation . $age . $email . $phone . $arrival; 
-
-file_put_contents($file, $data, FILE_APPEND);
 
 ?>
 
@@ -58,7 +61,7 @@ file_put_contents($file, $data, FILE_APPEND);
 		<label for="first-name">First name:</label>
 		<input type="text" id="first-name" name="first-name" pattern="[A-Za-z]{1,}" required placeholder="Enter your first name"><br>
 		<label for="middle-name">Middle name:</label>
-		<input type="text" id="middle-name" name="middle-name" pattern="[A-Za-z]{1,}" placeholder="Enter your middle name"><br>
+        <input type="text" id="middle-name" name="middle-name" pattern="[A-Za-z]{1,}" placeholder="Enter your middle name"><br>
 		<label for="last-name">Last name:</label>
 		<input type="text" id="last-name" name="last-name" pattern="[A-Za-z]{1,}" required placeholder="Enter your last name"><br>
 		<label for="salutation-select">Choose salutation:</label>
