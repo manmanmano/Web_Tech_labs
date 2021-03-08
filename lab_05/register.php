5d7def5fe5b31fd357774edc7ea5d7862bd177df
Mariano <madang@taltech.ee>
2021-03-08 16:31:53 +0200

    added validation

--- lab_05/register.php
+++ lab_05/register.php
@@ -1,13 +1,47 @@
 <?php 
+
 $file = 'csvfile/data.csv'; 
-$names = $_POST['first-name'] . chr(44) . $_POST['middle-name'] . chr(44) . $_POST['last-name'] . chr(44);
+
+if (preg_match("/^[a-zA-Z']+$/", $_POST['first-name'])) {
+	$first = $_POST['first-name']; . chr(44);
+} else {
+	die('Invalid first name given!');
+}
+
+if (preg_match("/^[a-zA-Z']+$/", $_POST['middle-name'])) {
+	$middle = $_POST['middle-name']; . chr(44);
+} else {
+	die('Invalid second name given!');
+}
+
+if (preg_match("/^[a-zA-Z']+$/", $_POST['last-name'])) {
+	$last = $_POST['last-name']; . chr(44);
+} else {
+	die('Invalid last name given!');
+}
+
 $salutation = $_POST['salutation-select'] . chr(44); 
-$age = $_POST['age'] . chr(44);
-$email = $_POST['email'] . chr(44);
+
+if ($_POST['age'] > 17 && is_numeric($_POST['age'])) {
+	$age = $_POST['age'] . chr(44);
+} else {
+	die('Invalid age given!'); 
+}
+
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $email = $_POST['email'] . chr(44);
+} else {
+    die('Invalid email given!');
+}
+
 $phone = $_POST['contact-phone'] . chr(44);
+
 $arrival = $_POST['arrival'] . PHP_EOL;
+
 $data = $names . $salutation . $age . $email . $phone . $arrival; 
+
 file_put_contents($file, $data, FILE_APPEND);
+
 ?>
 
 <!DOCTYPE html>
