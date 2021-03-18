12f79f44dae58395c6026a7fee2f2d2eacd533ce
Mariano D'Angelo <madang@ttu.ee>
2021-03-18 18:23:01 +0200

    no prog at all

--- lab_06/CoursesClass.php
+++ lab_06/CoursesClass.php
@@ -7,18 +7,11 @@ class Course {
     public $name;
     public $points;
     public $semester;
-
-    function __construct($code, $name, $points, $semester) {
-        $this -> code = $code;
-        $this -> name = $name;
-        $this -> points = $points;
-        $this -> semester = $semester;
 }
 
-function fileSplit($handle, $file) {
-    $arr = [];
-    for ($i = 0; $i < filesize($file); $i++) {
-        for ($j = 0; $j != PHP.EOL; $j++) {
+function getCoursesTable() {
+
+}
 
 
 $file = 'data/courses.csv';
@@ -26,4 +19,6 @@ if (!file_exists($file)) {
     die("FILE NOT FOUND");
 }
 $handle = fopen($file, 'r');
+
+fclose($handle);
 ?>
