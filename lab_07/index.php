50398ea5b8e80a24dbfa8f5e52585326eacb8179
Mariano D'Angelo <madang@ttu.ee>
2021-03-29 21:45:19 +0300

    info displayed and structure changed

--- lab_07/index.php
+++ lab_07/index.php
@@ -1,5 +1,6 @@
 <?php
 include_once('cookies.php');
+include_once('form.php');
 include_once('sessions.php');
 ?>
 
--- lab_07/sessions.php
+++ lab_07/sessions.php
@@ -1,37 +1,16 @@
-<!DOCTYPE html>                                                                 
-<html lang="en">                                                                
-<head>                                                                          
-    <meta charset="utf-8">                                                      
-    <title>Index</title>                                                        
-</head>                                                                         
-<body>                                                                          
-    <br><br>                                                                    
-    <form action="#" method="POST" id="data">                        
-    <label for="pwd">PIN:</label>                                               
-    <input type="password" name="password" minlength="4" maxlength="8"          
-        required placeholder="Insert a PIN of 4-8 symbols">                     
-    <input type="submit" name="submit" value="Log in">                          
-    </form>                                                                     
-</body>                                                                         
-</html>    
-
 <?php
+function startSession($name, $age, $location) {
+    session_name("UserInterface");
+    session_start();
+    $_SESSION['name'] = $name;
+    $_SESSION['age'] = $age;
+    $_SESSION['location'] = $location;
+    echo "Name of the user: ", $_SESSION['name'], "<br>";
+    echo "Age of the user: ", $_SESSION['age'], "<br>";
+    echo "Location of the user: ", $_SESSION['location'], "<br>";
+    if (isset($_SESSION['name']) && isset($_SESSION['age']) 
+        && isset($_SESSION['location'])) {
 
-function establishSession($name, $age, $location) {
-    
-}
-
-if (isset($_POST['submit'])) {
-    
-    if (strlen($_POST['password']) < 4 && strlen($_POST['password']) > 8) {
-        die("INVALID INPUT!");
-    }
-    
-    if (!strcmp($_POST['password'], "m@m@M14")) {
-        echo "Log in successful!";
-        establishSession("Mariano", 20, "Tallinn");
-    } else {
-        echo "Incorrect PIN, plese retry.";
     }
 
 }
