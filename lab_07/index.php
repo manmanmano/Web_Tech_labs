b08847e9f00a7e2cb25b675cedab21666fa850e5
Mariano D'Angelo <madang@ttu.ee>
2021-03-29 17:29:19 +0300

    added longTimeCount

--- lab_07/index.php
+++ lab_07/index.php
@@ -3,11 +3,24 @@ $transCookie = "ctransient";
 $myCookie = "Mariano";
 setcookie($transCookie, $myCookie);
 if (!isset($_COOKIE[$transCookie])) {
-    echo "There is no cookie named ", $transCookie;
+    echo $transCookie, ' = FALSE', PHP_EOL;
 } else {
-    echo "Cookie '", $transCookie, "' = '", $myCookie, "'";
+    echo $transCookie, ' = TRUE', PHP_EOL;
 }
 
 $persShortCookie = "ShortTimeCount";
 setcookie($persShortCookie, 0, time()+120);
+if (!isset($_COOKIE[$persShortCookie])) {
+    echo $persShortCookie, ' = FALSE', PHP_EOL;
+} else {
+    echo $persShortCookie, ' = TRUE', PHP_EOL;
+}
+
+$persLongCookie = "LongTimeCount";
+setcookie($persLongCookie, 0, time()+3600);
+if (!isset($_COOKIE[$persLongCookie])) {
+    echo $persLongCookie, ' = FALSE', PHP_EOL;
+} else {
+    echo $persLongCookie, ' = TRUE', PHP_EOL;
+}
 ?>
