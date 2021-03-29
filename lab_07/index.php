fc35692362f2722d05374a7073513cf4e3a71f9e
Mariano D'Angelo <madang@ttu.ee>
2021-03-29 18:35:06 +0300

    done shortTime and LongTime. counter works

--- lab_07/index.php
+++ lab_07/index.php
@@ -1,10 +1,30 @@
 <?php 
 $cookieName = "ctransient";
 $cookieValue = "Mariano";
-setcookie($cookieName, $cookieValue);
+
 if (!isset($_COOKIE[$cookieName])) {
     echo "There is no cookie named ", $cookieName, "<br>";
 } else {
     echo $cookieName, ' = ', $cookieValue, "<br>";
 }
+
+if (isset($_COOKIE['ShortTimeCount'])) {
+    $accessCount = $_COOKIE['ShortTimeCount'] + 1;
+    echo "ShortTimeCount = ", $accessCount, "<br>";
+} else {
+    $accessCount = 1;
+    echo "ShortTimeCount = ", $accessCount, "<br>";
+}
+
+if (isset($_COOKIE['LongTimeCount'])) {
+    $accessCount = $_COOKIE['LongTimeCount'] + 1;
+    echo "LongTimeCount = ", $accessCount, "<br>";
+} else {
+    $accessCount = 1;
+    echo "LongTimeCount = ", $accessCount, "<br>";
+}
+
+setcookie($cookieName, $cookieValue);
+setcookie('ShortTimeCount', $accessCount, time()+120);
+setcookie('LongTimeCount', $accessCount, time()+3600);
 ?>
