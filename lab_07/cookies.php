e41fe1c2ede176387ed8fd9b9c536504d15f3281
Mariano D'Angelo <madang@ttu.ee>
2021-03-29 20:04:57 +0300

    changed structure of the lab. splitted in files

---
+++ lab_07/cookies.php
@@ -0,0 +1,30 @@
+<?php
+$cookieName = "ctransient";
+$cookieValue = "Mariano";
+
+if (!isset($_COOKIE[$cookieName])) {
+    echo "There is no cookie named ", $cookieName, "<br>";
+} else {
+    echo $cookieName, ' = ', $cookieValue, "<br>";
+}
+
+if (isset($_COOKIE['ShortTimeCount'])) {
+    $shortCount = $_COOKIE['ShortTimeCount'] + 1;
+    echo "ShortTimeCount = ", $shortCount, "<br>";
+} else {
+    $shortCount = 1;
+    echo "ShortTimeCount = ", $shortCount, "<br>";
+}
+
+if (isset($_COOKIE['LongTimeCount'])) {
+    $longCount = $_COOKIE['LongTimeCount'] + 1;
+    echo "LongTimeCount = ", $longCount, "<br>";
+} else {
+    $longCount = 1;
+    echo "LongTimeCount = ", $longCount, "<br>";
+}
+
+setcookie($cookieName, $cookieValue);
+setcookie('ShortTimeCount', $shortCount, time()+120);
+setcookie('LongTimeCount', $longCount, time()+3600);
+?>
---
+++ lab_07/sessions.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>                                                                    
+<html lang="en">                                                                   
+<head>                                                                             
+    <meta charset="utf-8">                                                         
+    <title>Index</title>                                                           
+</head>                                                                            
+<body>                                                                             
+    <br>                                                                        
+    <form action="#" method="POST" id="data">                                      
+    <label for="pwd">PIN:</label>                                                  
+    <input type="password" name="password" minlength="4" maxlength="8"             
+        required placeholder="Insert a PIN of 4-8 symbols">                        
+    <input type="submit" name="submit" required placeholder="Log in">              
+    </form>                                                                        
+</body>                                                                            
+</html>  
