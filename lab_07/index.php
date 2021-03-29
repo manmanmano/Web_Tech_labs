69bd4e2ff367761cead4f09d1b2d3a42897608fa
Mariano D'Angelo <madang@ttu.ee>
2021-03-29 17:35:41 +0300

    formatted

--- lab_07/index.php
+++ lab_07/index.php
@@ -3,24 +3,30 @@ $transCookie = "ctransient";
 $myCookie = "Mariano";
 setcookie($transCookie, $myCookie);
 if (!isset($_COOKIE[$transCookie])) {
-    echo $transCookie, ' = FALSE', PHP_EOL;
+    echo $transCookie, ' = FALSE';
+    echo "<br>";
 } else {
-    echo $transCookie, ' = TRUE', PHP_EOL;
+    echo $transCookie, ' = TRUE';
+    echo "<br>";
 }
 
 $persShortCookie = "ShortTimeCount";
 setcookie($persShortCookie, 0, time()+120);
 if (!isset($_COOKIE[$persShortCookie])) {
-    echo $persShortCookie, ' = FALSE', PHP_EOL;
+    echo $persShortCookie, ' = FALSE';
+    echo "<br>";
 } else {
-    echo $persShortCookie, ' = TRUE', PHP_EOL;
+    echo $persShortCookie, ' = TRUE';
+    echo "<br>";
 }
 
 $persLongCookie = "LongTimeCount";
 setcookie($persLongCookie, 0, time()+3600);
 if (!isset($_COOKIE[$persLongCookie])) {
-    echo $persLongCookie, ' = FALSE', PHP_EOL;
+    echo $persLongCookie, ' = FALSE';
+    echo "<br>";
 } else {
-    echo $persLongCookie, ' = TRUE', PHP_EOL;
+    echo $persLongCookie, ' = TRUE';
+    echo "<br>";
 }
 ?>
