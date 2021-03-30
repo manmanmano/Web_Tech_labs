5391987953f3d037cc70edaf47e5ef08a3f3e7e7
Mariano D'Angelo <madang@ttu.ee>
2021-03-30 13:52:03 +0300

    nothing works

--- lab_07/index.php
+++ lab_07/index.php
@@ -1,29 +1,23 @@
 <?php
 include_once('cookies.php');                                                                           
                                                                                 
-if (isset($_GET['submit'])) {                                                   
+if (strlen($_GET['password']) < 4 && strlen($_GET['password']) > 8) {          
+    die('INVALID INPUT!');                                                  
+}                                                                           
                                                                                 
-    if (strlen($_GET['password']) < 4 && strlen($_GET['password']) > 8) {          
-        die('INVALID INPUT!');                                                  
-    }                                                                           
-                                                                                
-    if (!strcmp($_GET['password'], '123456')) {                                 
-        echo "Log in successful! <br>";                                         
-        session_name("Mariano");                                                
-        session_start();                                                        
-        echo "<br>";                                                            
-        if (!isset($_SESSION['name']) && !isset($_SESSION['age']) &&            
-            !isset($_SESSION['location']) && !isset($_SESSION['counter'])) {    
-            $_SESSION['name'] = 'Mariano';                                      
-            $_SESSION['age'] = 20;                                              
-            $_SESSION['location'] = 'Tallinn';                                  
-            $_SESSION['counter'] = 1;                                           
-        }                                                                       
-                                                                                
-    }                                                                           
-                                                                                
-}                                                                               
-                                                                                
+if (!strcmp($_GET['password'], '123456')) {                                 
+    session_name("Mariano");                                                
+    session_start();                                                        
+    echo "<br>";                                                            
+    if (!isset($_SESSION['name']) && !isset($_SESSION['age']) &&            
+        !isset($_SESSION['location']) && !isset($_SESSION['counter'])) {    
+        $_SESSION['name'] = 'Mariano';                                      
+        $_SESSION['age'] = 20;                                              
+        $_SESSION['location'] = 'Tallinn';                                  
+        $_SESSION['counter'] = 1;                                           
+    }                                                                       
+
+}                                                                           
 ?>
 
 <!DOCTYPE html>
@@ -43,3 +37,30 @@ if (isset($_GET['submit'])) {
 </body>
 </html>
 
+<?php
+if (isset($_GET['submit'])) {
+    session_name("Mariano");
+    session_start();
+    if (isset($_SESSION['name']) && !isset($_SESSION['age']) &&
+        isset($_SESSION['location']) && isset($_SESSION['counter'])) {
+        $_SESSION['name'] = "Mariano";
+        $_SESSION['age'] = 20;
+        $_SESSION['location'] = "Tallinn";
+        echo "Name of the user: ", $_SESSION['name'], "<br>";
+        echo "Age of the user: ", $_SESSION['age'], "<br>";
+        echo "Location of the user: ", $_SESSION['location'], "<br>";
+        echo "<br> Session number ", $_SESSION['counter']++;
+        echo "<br><a href='reset.php'>Reset the session counter here</a><br>";
+        echo "<br>
+            <form action='#' method='GET'>
+                <input type='submit' value='Log out' name='logout'>
+            </form>";
+        if (isset($_GET['logout'])) {
+            session_unset();
+            session_destroy();
+        } 
+    } else {
+        $_SESSION['counter'] = 1;
+    }
+}
+?>
