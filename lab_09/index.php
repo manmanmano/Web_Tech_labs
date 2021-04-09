9828134628c2605846c5adcc4935202d791b5a82
Mariano D'Angelo <madang@ttu.ee>
2021-04-09 12:05:32 +0300

    courses retrieved. not shown in output

--- lab_09/index.php
+++ lab_09/index.php
@@ -1,12 +1,24 @@
 <?php
 
-require_once "dbconnection_data.php";
+include_once "dbconnection_data.php";
+
+function listCourses($result, $query) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+            printf("%s\t %s\t %s<br>", $row["course_code"], $row["course_name"],
+                $row["ects_credit"]);
+        }
+    } else {
+        echo "No courses found!";
+    }
+}
 
 $link = mysqli_connect($server, $user, $password, $database);
-if (!$link) 
-    die("Connection to DB failed: " . mysqli_connect_error());
-else 
-    echo "Connection to DB successful!";
+if (!$link) die ("Connection to DB failed: " . mysqli_connect_error());
+else echo "Connection established!";
 
+$coursesQuery = "SELECT course_code, course_name, ects_credits FROM courses;";
+$result = mysqli_query($link, $coursesQuery);
 
+listCourses($result, $coursesQuery);
 ?>
