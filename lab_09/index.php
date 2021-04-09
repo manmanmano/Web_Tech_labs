c391f7a02a35176bdcc8993db2fde7aa5fbaaa0e
Mariano D'Angelo <madang@ttu.ee>
2021-04-09 12:21:23 +0300

    courses works

--- lab_09/index.php
+++ lab_09/index.php
@@ -4,10 +4,12 @@ include_once "dbconnection_data.php";
 
 function listCourses($result, $query) {
     if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-            printf("%s\t %s\t %s<br>", $row["course_code"], $row["course_name"],
-                $row["ects_credit"]);
+        echo "<pre>";
+        while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+            printf("%s\t %s\t %s<br>", $row[0], $row[1],
+                $row[2]);
         }
+        echo "</pre>";
     } else {
         echo "No courses found!";
     }
@@ -15,7 +17,7 @@ function listCourses($result, $query) {
 
 $link = mysqli_connect($server, $user, $password, $database);
 if (!$link) die ("Connection to DB failed: " . mysqli_connect_error());
-else echo "Connection established!";
+else echo "Connection established!<br><br>";
 
 $coursesQuery = "SELECT course_code, course_name, ects_credits FROM courses;";
 $result = mysqli_query($link, $coursesQuery);
