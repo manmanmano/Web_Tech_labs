31c3df1288464d14638aa2a1f2af0267e5b19f10
Mariano D'Angelo <madang@ttu.ee>
2021-04-09 15:52:08 +0300

    retrived semesters

--- lab_09/index.php
+++ lab_09/index.php
@@ -17,11 +17,26 @@ function listCourses($result, $query) {
     }
 }
 
+function listSemesters($result, $query) {
+    if (mysqli_num_rows($result) > 0) {
+        echo "<pre>";
+        while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+            printf("<a href=''>%s</a>\t", $row[0]);
+        }
+        echo "</pre>";
+    } else {
+        echo "No semesters found!";
+    }
+}
+
 $link = mysqli_connect($server, $user, $password, $database);
 if (!$link) die ("Connection to DB failed: " . mysqli_connect_error());
 
 $coursesQuery = "SELECT course_code, course_name, ects_credits FROM courses;";
-$result = mysqli_query($link, $coursesQuery);
+$coursesResult = mysqli_query($link, $coursesQuery);
+
+$semestersQuery = "SELECT semester_name FROM semesters_201752;";
+$semestersResult = mysqli_query($link, $semestersQuery);
 
 ?>
 
@@ -33,13 +48,25 @@ $result = mysqli_query($link, $coursesQuery);
         <link rel="stylesheet" type="text/css" href="styles/style.css">
     </head>
     <body>
+        <header>                                                                
+            <nav>                                                               
+                <ul>                                                            
+                    <li class="menu">
+                    <?php 
+                    listSemesters($semestersResult, $semestersQuery);
+                    ?>
+                    </li>           
+                </ul>                                                           
+            </nav>                                                              
+        </header>  
+        <?php listSemesters($semestersResult, $semestersQuery)?>
         <table>
             <tr>
                 <th>Course Code</th>
                 <th>Course Name</th>
                 <th>Credits</th>
             </tr>
-            <?php  listCourses($result, $coursesQuery)?>
+            <?php listCourses($coursesResult, $coursesQuery)?>
         </table>
     </body>
 </html>
