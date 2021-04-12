5b79ddf9ef9c619db09c98599e10322424042a24
Mariano D'Angelo <madang@ttu.ee>
2021-04-12 15:51:49 +0300

    everything works

--- lab_09/index.php
+++ lab_09/index.php
@@ -37,22 +37,41 @@ function listCourses($link) {
 
     $sort = "ASC";
     if (isset($_GET['sortBy'])) {
+        
         if ($_GET['sortBy'] == 'ASC') {
             $sort = "DESC";
         } else {
             $sort = "ASC";
         }
+    
+        if ($_GET['field'] == 'course_code') {
+            $field = 'course_code';
+        } elseif ($_GET['field'] == 'course_name') {
+            $field = 'course_name';
+        } elseif ($_GET['field'] == 'ects_credits') {
+            $field = 'ects_credits';
+        } elseif ($_GET['field'] == 'semester_name') {
+            $field = 'semester_name';
+        }
+
+        $safeSort = sanitizeInputVar($link, $sort);
+        $safeField = sanitizeInputVar($link, $field);
+        $query = mysqli_prepare($link, 
+            "SELECT course_code, course_name, ects_credits, semester_name
+            FROM courses AS C, semesters_201752 AS S
+            WHERE C.Semesters_ID=S.ID
+            ORDER BY " .  $field . " " .  $sort . ";");
     }
-   
+
     mysqli_stmt_execute($query);
     mysqli_stmt_bind_result($query, $course_code, $course_name, $ects_credits, $semester_name);
     
     echo "
      <tr>
-        <th><a href='index.php?sortBy=" . $sort . "'>Course Code</a></th>
-        <th><a href='index.php?sortBy=" . $sort . "'>Course Name</a></th>
-        <th><a href='index.php?sortBy=" . $sort . "'>Credits</a></th>
-        <th><a href='index.php?sortBy=" . $sort . "'>Semester</a></th>
+        <th><a href='index.php?sortBy=" . $safeSort . "&field=course_code'>Course Code</a></th>
+        <th><a href='index.php?sortBy=" . $safeSort . "&field=course_name'>Course Name</a></th>
+        <th><a href='index.php?sortBy=" . $safeSort . "&field=ects_credits'>Credits</a></th>
+        <th><a href='index.php?sortBy=" . $safeSort . "&field=semester_name'>Semester</a></th>
      </tr>";
     while (mysqli_stmt_fetch($query)){
         echo "
