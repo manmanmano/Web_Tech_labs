f4351270d529f7ac48256f4235f07ab263998222
Mariano D'Angelo <madang@ttu.ee>
2021-04-14 18:42:12 +0300

    cookie works

--- lab_10/shoppinglist.js
+++ lab_10/shoppinglist.js
@@ -1,1 +1,9 @@
-document.cookie = "username=" + username + ";path=/~madang/Web_Technologies/lab_10/";
+function createCookie() {
+    var username = document.myForm.username.value;
+    if (username == "") {
+        alert("Input left blank!");
+        return;
+    }
+     document.cookie = "username=" + username + ";path=/~madang/Web_Technologies/lab_10/";
+}
+
