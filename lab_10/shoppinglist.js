967dbf37ae654f12695311afffc31e8cfe7da909
Mariano D'Angelo <madang@ttu.ee>
2021-04-15 11:44:50 +0300

    step 1 DONE

--- lab_10/shoppinglist.js
+++ lab_10/shoppinglist.js
@@ -1,12 +1,17 @@
-var userName = prompt("Please enter your name");
-while (true) {
-    if (userName === "") {
-        alert("Input left blank!");
-        userName = prompt("Please enter your name");
-    } else {
-        break;
+function checkInput(userName) {
+    while (true) {
+        if (userName === "") {
+            alert("input left blank!");
+            userName = prompt("please enter your name");
+        } else {
+            break;
+        }
     }
+    return userName;
 }
+
+var userName = prompt("Please enter your name");
+userName = checkInput(userName);
 document.cookie = "name=" + userName + ";path=/~madang/Web_Technologies/lab_10/;";
 sessionStorage.setItem("name", userName);
 document.getElementById("userName").innerHTML = userName + "'s Shopping List";
