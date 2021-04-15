31150b4d12e83f7d0d0fc10f19b962c8403258d8
Mariano D'Angelo <madang@ttu.ee>
2021-04-15 10:12:19 +0300

    added prompt instead of form. restarting

--- lab_10/shoppinglist.js
+++ lab_10/shoppinglist.js
@@ -1,9 +1,3 @@
-function createCookie() {
-    var name = document.myForm.name.value;
-    if (name === "") {
-        alert("Input left blank!");
-        return;
-    }
-    document.cookie = "name=" + name + ";path=/~madang/Web_Technologies/lab_10/;";
-    sessionStorage.setItem("name", name);
-}
+var personName = prompt("Please enter your name");
+document.cookie = "name=" + personName + ";path=/~madang/Web_Technologies/lab_10/;";
+sessionStorage.setItem("name", personName);
