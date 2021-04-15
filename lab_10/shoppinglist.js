fae7da24111f735563ac98748ddefc2847bfb1f6
Mariano D'Angelo <madang@ttu.ee>
2021-04-15 10:56:26 +0300

    input cannot be blank

--- lab_10/shoppinglist.js
+++ lab_10/shoppinglist.js
@@ -1,3 +1,11 @@
 var personName = prompt("Please enter your name");
+while (true) {
+    if (personName === "") {
+        alert("Input left blank!");
+        personName = prompt("Please enter your name");
+    } else {
+        break;
+    }
+}
 document.cookie = "name=" + personName + ";path=/~madang/Web_Technologies/lab_10/;";
 sessionStorage.setItem("name", personName);
