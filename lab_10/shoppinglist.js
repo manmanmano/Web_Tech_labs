8ffff2b4365cbbcb2778437b27ff739a37c7b7dd
Mariano D'Angelo <madang@ttu.ee>
2021-04-21 17:43:55 +0300

    first step done. same as before

---
+++ lab_10/shoppinglist.js
@@ -0,0 +1,22 @@
+function checkInput(user) {
+    while (true) {
+        if (user === "" || user === null) {
+            alert("Input left blank!");
+            user = prompt("Please enter your name");
+        } else {
+            break;
+        }
+    }
+    return user;
+}
+
+if (document.cookie == "") {
+    var name = prompt("Please enter your name: ");
+    name = checkInput(name);
+    if (name != "" && name != null) {
+        document.cookie = "name=" + name + "; path=/~madang/Web_Technologies/lab_10/;";
+    }
+}
+
+sessionStorage.setItem("name", name);
+document.getElementById("user").innerHTML = name + "'s Shopping List";
