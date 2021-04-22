4d6ff0a3d6e593899ab2adcbe07f6ae47db4924b
Mariano D'Angelo <madang@ttu.ee>
2021-04-22 13:35:41 +0300

    added getCookie function

--- lab_10/shoppinglist.js
+++ lab_10/shoppinglist.js
@@ -1,3 +1,18 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 function logout() {
     document.cookie = "name=''; expires=Mon, 31 Dec 2018 12:00:00 UTC; path=/~madang/Web_Technologies/lab_10/;";
